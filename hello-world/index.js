6f0197e95fe5fc41a6bb9c915f60f9057e3a68b2
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2023-12-07 02:16:29 +0530

    Level-2 milestone uploaded

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,5 +1,19 @@
-const hello = () =>{
-console.log("hello world");
-}
-hello();
+// const readline = require('readline')
 
+// const lineDetail = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// })
+
+// lineDetail.question('Please provide your name - ', (name) => {
+//   console.log(`Hi ${name}!`)
+//   lineDetail.close()
+// })
+// const args = require('minimist')(process.argv.slice(2))
+import minimist from 'minimist'
+const args = minimist(process.argv.slice(2), {
+  default: {
+    greeting: 'Hello'
+  }
+})
+console.log(args)
