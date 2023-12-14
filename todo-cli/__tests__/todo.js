8672f7f52622909a634ccc69a6fba00c1bdc53ae
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2023-12-14 23:58:29 +0530

    Add pre-commit hook to run tests

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,18 @@
+const todoList=require("../todo");
+const {all,markAsComplete,add}=todoList();
+describe("first test suites",() => {
+    test("first test case",()=>{
+        expect(all.length).toBe(0)
+        add({
+            title:"First test case",
+            completed:false,
+            dueDate: new Date().toISOString().slice(0,10)
+        })
+        expect(all.length).toBe(1)
+    });
+    test("second test",()=>{
+        expect(all[0].completed).toBe(false)
+        markAsComplete(0);
+        expect(all[0].completed).toBe(true)
+    });
+});
