63b4b5dbfdcefb83b4ea8c1d0a9b219dd18f72d4
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2023-12-17 23:12:49 +0530

    database

--- todo-cli/assert.js
+++ todo-cli/assert.js
@@ -1,19 +1,18 @@
 // assert.js
 
 let toggleTodoCompletedStatus = (todoItem) => {
-    todoItem.completed=!todoItem.completed;
-    return todoItem;
+  todoItem.completed = !todoItem.completed;
+  return todoItem;
+};
+
+let testToggleCompletion = () => {
+  let item = {
+    title: "Buy Milk",
+    completed: false,
   };
-  
-  let testToggleCompletion = () => {
-    let item = {
-      title: 'Buy Milk',
-      completed: false,
-    }
-    item = toggleTodoCompletedStatus(item);
-  
-    console.assert(item.completed === true, 'Todo item should be completed');
-  
-  }
-  
-  testToggleCompletion()
+  item = toggleTodoCompletedStatus(item);
+
+  console.assert(item.completed === true, "Todo item should be completed");
+};
+
+testToggleCompletion();
